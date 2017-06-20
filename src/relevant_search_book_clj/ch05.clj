bb078e852b433ed4feed263daa23b4688977c28a
Ivan Sharamet <isharamet@gmail.com>
2017-06-20 12:10:46 +0300

    Add first part of the code for chapter 5

--- src/relevant_search_book_clj/ch05.clj
+++ src/relevant_search_book_clj/ch05.clj
@@ -1,1 +1,31 @@
-(ns relevant-search-book-clj.ch05)
+(ns relevant-search-book-clj.ch05
+  (:require [relevant-search-book-clj.core :refer :all]
+            [qbits.spandex :as spandex]))
+
+
+(def index-data
+  (reduce
+    (fn [acc movie]
+      (let [[id data] movie]
+        (conj
+          acc
+          {:index {:_index "tmdb", :_type "movie", :_id id}}
+          data)))
+    []
+    tmdb))
+
+(defn get-movie-by-id
+  [id]
+  (spandex/request-async
+    es-client
+    {:url (str "/tmdb/movie/" id)
+     :method :get
+     :success (fn [rs] (clojure.pprint/pprint (:body rs)))
+     :error   (fn [ex] (println ex))}))
+;; ---
+
+(reindex index-data)
+
+(def space-jam-id 2300)
+
+(get-movie-by-id space-jam-id)
